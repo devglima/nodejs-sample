820d043a0cb64c07638333916d47ba5a1a67d450
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-25 16:30:28 -0300

    Mongo connection issue

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false");
+const connection_url =
+    "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false";
+mongoose.connect(connection_url);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     return res.json({ message: "Server is up!" });
 });
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     var foods = db.foods.find({}).pretty();
     return res.json({ message: foods });
 });
