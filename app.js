a29ba40c45f6e11c0efae047963186572a41bea2
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-25 18:22:33 -0300

    Test schema

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const foodsModel = require("./models/foods");
 const app = express();
 app.use(express.json());
 
 const connection_url =
     "mongodb://root:SimoniniDB@b2b-db.cf9vntua4zgb.us-east-1.docdb.amazonaws.com:27017/b2b-db?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
 
-var db = async () => await mongoose.connect(connection_url, {
+async () => await mongoose.connect(connection_url, {
     ssl: true,
     sslValidate: false,
     connectTimeoutMS: 100000,
@@ -36,8 +37,8 @@ app.get("/", (req, res) => {
 
 app.get("/", (req, res) => {
     console.log(res.statusCode);
-    var foods = db.foods.find({}).pretty();
-    return res.json({ message: foods });
+    const foods = await userModel.find({});
+    return res.send(foods);
 }).on('error', function (error) {
     console.log(error.message);
 });;
