20ef47496756f062733fb252a0465a8230621329
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-25 18:23:37 -0300

    Test schema

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const connection_url =
     "mongodb://root:SimoniniDB@b2b-db.cf9vntua4zgb.us-east-1.docdb.amazonaws.com:27017/b2b-db?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
 
-async () => await mongoose.connect(connection_url, {
+var db = async () => await mongoose.connect(connection_url, {
     ssl: true,
     sslValidate: false,
     connectTimeoutMS: 100000,
