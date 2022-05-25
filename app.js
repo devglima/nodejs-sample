99f8ae3c621bb09b8e82dee47dd0f355e13dd3e6
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-25 16:22:08 -0300

    /foods added

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
+mongoose.connect("mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false");
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.get("/", (req,res) => {
     return res.json({ message: "Server is up!" });
+});
+
+app.get("/", (req,res) => {
+    var foods = db.foods.find({}).pretty();
+    return res.json({ message: foods });
 });
 
 app.listen(3000);
