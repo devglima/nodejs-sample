ab7b575f55e27e70b94c745e20334fa024b2a9f4
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-26 15:20:11 -0300

    Foods added test id 46

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.get("/categories/:id", async (req, res) => {
     console.log(res.statusCode);
     const id = req.params.id;
     console.log(id);
-    var categories = await categoriesModel.aggregate([{$match: { id: id }}, { $lookup: { from: "foods", localField: "id", foreignField: "category_id", as: "foods" }}]); 
+    var categories = await categoriesModel.aggregate([{$match: { id: 46 }}, { $lookup: { from: "foods", localField: "id", foreignField: "category_id", as: "foods" }}]); 
     console.log(categories);
     return res.status(200).json(categories);
 }).on('error', function (error) {
