26f20cc7c90ff6e2ba04422bd209ae5ce55c52a3
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-26 00:30:12 -0300

    Db name aws

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const foodsModel = require("./models/foods.js");
 app.use(express.json());
 
 const connection_url =
-    "mongodb://root:SimoniniDB@127.0.0.1:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
+    "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
 
 (async () => await mongoose.connect(connection_url, {
     ssl: true,
