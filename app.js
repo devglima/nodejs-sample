0428b0a4691d534385ac9b4b7fdd13ba9e097e08
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-26 15:32:05 -0300

    Lookup working

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const foodsModel = require("./models/foods.js");
 const categoriesModel = require("./models/categories.js");
 
+const app = express();
 app.use(express.json());
 
-const connection_url =
-    "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
+const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
 
 (async () => await mongoose.connect(connection_url, {
     ssl: true,
@@ -31,36 +30,30 @@ const connection_url =
     }))();
 
 app.get("/", (req, res) => {
-    console.log(res.statusCode);
     return res.json({ message: "Server is up!" });
 }).on('error', function (error) {
-    console.log(error.message);
+    return res.status(404).json({ "Error" : error.message });
 });
 
 app.get("/foods", async (req, res) => {
-    console.log(res.statusCode);
     foodsModel.find((err, foods) => {
         res.status(200).json(foods);
     });
 }).on('error', function (error) {
-    console.log(error.message);
+    return res.status(404).json({ "Error" : error.message });
 });
 
 app.get("/categories", async (req, res) => {
-    console.log(res.statusCode);
     categoriesModel.find((err, categories) => {
         res.status(200).json(categories);
     });
 }).on('error', function (error) {
-    console.log(error.message);
+    return res.status(404).json({ "Error" : error.message });
 });
 
 app.get("/categories/:id", async (req, res) => {
-    console.log(res.statusCode);
     var categoryID = parseInt(req.params.id);
-    console.log(categoryID);
-    var categories = await categoriesModel.aggregate([{$match: { id: categoryID }}, { $lookup: { from: "foods", localField: "id", foreignField: "category_id", as: "foods" }}]).lean(); 
-    console.log(categories);
+    var categories = await categoriesModel.aggregate([{$match: { id: categoryID }}, { $lookup: { from: "foods", localField: "id", foreignField: "category_id", as: "foods" }}]); 
     return res.status(200).json(categories);
 }).on('error', function (error) {
     console.log(error.message);
