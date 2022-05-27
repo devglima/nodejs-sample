dc14d8b24a54bfcfdf02ae575d62c25607f60c82
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-27 12:12:55 -0300

    Responsabilidades

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,6 @@ const categories = require("./categoriesRoutes.js")
 const routes = (app) => {
     app.route('/').get((req, res) => {
         return res.json({ message: "Server is up!" });
-    }).on('error', function (error) {
-        return res.status(404).json({ "Error": error.message });
     });
 
     app.use(
