8c62eabc4a85d988e8532d7c2d0d55e54d8cae29
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-27 12:57:28 -0300

    dbConnect

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const db = require("./config/dbConnect.js");
 const routes = require("./routes/index.js");
 require("dotenv-safe").config();
 
-const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
+/* const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
 
 (async () => await mongoose.connect(connection_url, {
     ssl: true,
@@ -25,7 +26,12 @@ const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us
     .catch((err) => {
         console.log("MongoDB isn't connected!", err);
         return false;
-    }))();
+    }))(); */
+
+db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.once("open", () => {
+    console.log('conexão com o banco feita com sucesso')
+})
 
 const app = express();
 app.use(express.json());
---
+++ config/dbConnect.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us-east-1.docdb.amazonaws.com:27017/b2b?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false&directConnection=true";
+
+(async () => await mongoose.connect(connection_url, {
+    ssl: true,
+    sslValidate: false,
+    sslCA: '../rds-combined-ca-bundle.pem',
+    connectTimeoutMS: 100000,
+    keepAlive: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    maxIdleTimeMS: 270000,
+    minPoolSize: 2,
+    maxPoolSize: 4
+})
+    .then(() => {
+        console.log("MongoDB Connected!");
+        return true;
+    })
+    .catch((err) => {
+        console.log("MongoDB isn't connected!", err);
+        return false;
+    }))();
+
+let db = mongoose.connection;
+
+module.exports = db;
