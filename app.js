08f8f3b6026df10790f450b3920f2ded6020c6d2
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-05-26 23:21:43 -0300

    JWT init

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
 const foodsModel = require("./models/foods.js");
 const categoriesModel = require("./models/categories.js");
 
@@ -13,17 +14,17 @@ const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us
     sslValidate: false,
     sslCA: './rds-combined-ca-bundle.pem',
     connectTimeoutMS: 100000,
-    keepAlive: true, 
-    useUnifiedTopology: true, 
-    useNewUrlParser: true, 
-    maxIdleTimeMS: 270000, 
-    minPoolSize: 2, 
+    keepAlive: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    maxIdleTimeMS: 270000,
+    minPoolSize: 2,
     maxPoolSize: 4
 })
     .then(() => {
-    console.log("MongoDB Connected!");
-    return true;
-})
+        console.log("MongoDB Connected!");
+        return true;
+    })
     .catch((err) => {
         console.log("MongoDB isn't connected!", err);
         return false;
@@ -32,15 +33,27 @@ const connection_url = "mongodb://root:SimoniniDB@b2b-db.cluster-c34svjdft6iv.us
 app.get("/", (req, res) => {
     return res.json({ message: "Server is up!" });
 }).on('error', function (error) {
-    return res.status(404).json({ "Error" : error.message });
+    return res.status(404).json({ "Error": error.message });
 });
 
-app.get("/foods", async (req, res) => {
+app.post('/login', (req, res, next) => {
+    const id = 1; //esse id viria do banco de dados
+    const token = jwt.sign({ id }, {
+        expiresIn: 300 // expires in 5min
+    });
+    return res.status(200).json({ auth: true, token: token });
+});
+
+app.post('/logout', function(req, res) {
+    return res.status(200).json({ auth: false, token: null });
+});
+
+app.get("/foods", verifyJWT , async (req, res) => {
     foodsModel.find((err, foods) => {
         res.status(200).json(foods);
     });
 }).on('error', function (error) {
-    return res.status(404).json({ "Error" : error.message });
+    return res.status(404).json({ "Error": error.message });
 });
 
 app.get("/categories", async (req, res) => {
@@ -48,15 +61,26 @@ app.get("/categories", async (req, res) => {
         res.status(200).json(categories);
     });
 }).on('error', function (error) {
-    return res.status(404).json({ "Error" : error.message });
+    return res.status(404).json({ "Error": error.message });
 });
 
 app.get("/categories/:id", async (req, res) => {
     var categoryID = parseInt(req.params.id);
-    var categories = await categoriesModel.aggregate([{$match: { id: categoryID }}, { $lookup: { from: "foods", localField: "id", foreignField: "category_id", as: "foods" }}]); 
+    var categories = await categoriesModel.aggregate([{ $match: { id: categoryID } }, { $lookup: { from: "foods", localField: "id", foreignField: "category_id", as: "foods" } }]);
     return res.status(200).json(categories);
 }).on('error', function (error) {
     console.log(error.message);
 });;
+
+function verifyJWT(req, res, next){
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
+    
+    jwt.verify(token, function(err, decoded) {
+      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      
+      next();
+    });
+}
 
 app.listen(3000);
