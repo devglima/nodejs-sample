dc52d61b22ada7dcb3bd2671904f47623f4302fb
rosariodeveloper <rosariomassango@gmail.com>
2022-06-02 23:14:11 +0100

    caregories routes

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -6,13 +6,20 @@ import { authenticate } from '../App/Middleware/AuthMiddleware.js';
 import { CategoriesController } from '../App/Controllers/CategoriesController.js';
 
 import { createCategoryValidator } from '../App/Validators/CategoriesValidator.js';
-//import { validationResult } from 'express-validator';
 
 const Route = express.Router();
 
-Route.post('/categories', [validator(createCategoryValidator)])
-   //.get('/faq_categories', authenticate, CategoriesController.)
-   //.get('/foods', authenticate, getFoods)
-   .get('/categories/:id', authenticate, CategoriesController.show);
+Route.get('/categories', authenticate, CategoriesController.show)
+   .get('/show/:id', authenticate, CategoriesController.show)
+   .post(
+      '/categories/create',
+      [authenticate, validator(createCategoryValidator)],
+      CategoriesController.create
+   )
+   .post(
+      '/categories/update',
+      [authenticate, validator(createCategoryValidator)],
+      CategoriesController.update
+   );
 
 export default Route;
