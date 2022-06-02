377639642da3d564695ca70566d1906239acac6f
rosariodeveloper <rosariomassango@gmail.com>
2022-06-02 23:31:23 +0100

    categories

--- App/Controllers/CategoriesController.js
+++ App/Controllers/CategoriesController.js
@@ -72,6 +72,7 @@ export class CategoriesController {
                message: 'Category name already exists',
             });
 
+         delete request.body.id;
          await Categories.create(request.body);
 
          return response.status(200).json({
