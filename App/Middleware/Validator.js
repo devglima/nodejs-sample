c6c640b822fdfb57a1d20185a5a5752567a81f37
rosariodeveloper <rosariomassango@gmail.com>
2022-06-02 23:37:57 +0100

    validator id error

--- App/Middleware/Validator.js
+++ App/Middleware/Validator.js
@@ -1,11 +1,6 @@
-import * as yup from 'yup';
-
 const validator = (schema) => async (req, res, next) => {
    try {
-      await schema.validate(
-         { id: yup.string().optional(), ...req.body },
-         { strict: true }
-      );
+      await schema.validate(req.body, { strict: true });
       next();
    } catch (error) {
       return res.status(500).json({ type: error.type, message: error.message });
