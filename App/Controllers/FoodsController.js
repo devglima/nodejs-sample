99daae96bf3799d63bfc58b79656920b80deee50
rosariodeveloper <rosariomassango@gmail.com>
2022-06-03 13:25:30 +0100

    create food controller

--- App/Controllers/FoodsController.js
+++ App/Controllers/FoodsController.js
@@ -1,6 +1,43 @@
-/* import FoodsModel from '../Models/Foods.js';
+import Foods from '../Models/Foods.js';
 
-class FoodsController {}
+export class FoodsController {
+   static async index(req, res) {
+      try {
+         const foods = await Foods.find();
+         return res.status(422).json({
+            success: true,
+            data: foods,
+         });
+      } catch (error) {
+         return res.status(422).json({
+            success: false,
+            message: 'Foods not retrieved. Try again later',
+         });
+      }
+   }
 
-export default FoodsController;
- */
+   static async foodsWithOrders(req, res) {
+      try {
+         var foodWithOrders = await Foods.aggregate([
+            {
+               $lookup: {
+                  from: 'orders',
+                  localField: 'order_id',
+                  foreignField: 'id',
+                  as: 'orders',
+               },
+            },
+         ]);
+
+         return res.status(200).json({
+            success: true,
+            data: foodWithOrders,
+         });
+      } catch (error) {
+         return res.status(422).json({
+            success: false,
+            message: 'Foods not retrieved. Try again later',
+         });
+      }
+   }
+}
