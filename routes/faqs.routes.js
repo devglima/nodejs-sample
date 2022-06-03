316f768c879d0aa4cbf3a66cdf7d82a1ea40a49e
rosariodeveloper <rosariomassango@gmail.com>
2022-06-03 12:39:31 +0100

    create faqs routes

--- routes/faqs.routes.js
+++ routes/faqs.routes.js
@@ -4,8 +4,22 @@ import { Router } from 'express';
 import { authenticate } from '../App/Middleware/AuthMiddleware.js';
 import { FaqsController } from '../App/Controllers/FaqsController.js';
 
+import { createFaqValidator } from '../App/Validators/CreateFaqsValidator.js';
+
 const Route = Router();
 
-Route.get('/faqs', authenticate, FaqsController.index);
+Route.get('/faqs', authenticate, FaqsController.index)
+   .get('/faqs/show/:id', authenticate, FaqsController.show)
+   .delete('/faqs/delete/:id', authenticate, FaqsController.delete)
+   .post(
+      '/faqs/create',
+      [/* authenticate, */ createFaqValidator],
+      FaqsController.create
+   )
+   .put(
+      '/faqs/update/:id',
+      [authenticate, createFaqValidator],
+      FaqsController.update
+   );
 
 export default Route;
