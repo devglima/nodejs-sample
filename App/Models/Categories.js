8c076799d4ff0dcb6ea3282ffc8cd1e59068164e
rosariodeveloper <rosariomassango@gmail.com>
2022-06-07 23:23:58 +0100

    criando unique validator

--- App/Models/Categories.js
+++ App/Models/Categories.js
@@ -2,16 +2,23 @@ import mongoose from 'mongoose';
 
 const { Schema, model } = mongoose;
 
-const CategoriesSchema = new Schema({
-   id: {
-      type: Number,
-      required: true,
+const CategoriesSchema = new Schema(
+   {
+      id: {
+         type: Number,
+      },
+      name: {
+         type: String,
+         required: true,
+      },
    },
-   name: {
-      type: String,
-      required: true,
-   },
-});
+   {
+      timestamps: {
+         createdAt: 'created_at',
+         updatedAt: 'updated_at',
+      },
+   }
+);
 
 const Categories = model('categories', CategoriesSchema);
 export default Categories;
