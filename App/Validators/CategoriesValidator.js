9c8eaa9beda42b954f7319372a27143c28818455
rosariodeveloper <rosariomassango@gmail.com>
2022-06-07 23:24:33 +0100

    criando category request validator

--- App/Validators/CategoriesValidator.js
+++ App/Validators/CategoriesValidator.js
@@ -1,6 +1,30 @@
-//import { checkSchema } from 'express-validator';
 import * as yup from 'yup';
+import validators from '../Services/ValidatorService.js';
+import Categories from '../Models/Categories.js';
 
-export const createCategoryValidator = yup.object({
-   name: yup.string().required('Category name is required'),
-});
+validators(yup);
+
+export const categoryValidator = async (req, res, next) => {
+   const schema = yup.object({
+      name: yup
+         .string('Category must be a string')
+         .unique({
+            table: Categories,
+            column: 'name',
+            where: req.params.id
+               ? {
+                    _id: { $ne: req.params.id },
+                 }
+               : null,
+            message: 'Category already exists',
+         })
+         .required('Category name is required'),
+   });
+
+   try {
+      await schema.validate(req.body, { strict: true });
+      next();
+   } catch (error) {
+      return res.status(422).json({ type: error.type, message: error.message });
+   }
+};
