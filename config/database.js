0dc3f03d7128ca3a118c741234da0372e1bc3cbd
rosariodeveloper <rosariomassango@gmail.com>
2022-06-07 14:55:09 +0100

    database

--- config/database.js
+++ config/database.js
@@ -4,8 +4,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-/* const options = {
-   ssl: true,
+const options = {
+   /* ssl: true,
    sslValidate: false,
    sslCA: './rds-combined-ca-bundle.pem',
    connectTimeoutMS: 100000,
@@ -15,25 +15,11 @@ dotenv.config();
    useNewUrlParser: true,
    maxIdleTimeMS: 270000,
    minPoolSize: 2,
-   maxPoolSize: 4,
-}; */
+   maxPoolSize: 4, */
+};
 
 (async () =>
-   await mongoose.connect(process.env.DATABASE_URL, {
-      ssl: true,
-      sslValidate: false,
-      sslCA: './rds-combined-ca-bundle.pem',
-      connectTimeoutMS: 100000,
-      keepAlive: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      maxIdleTimeMS: 270000,
-      minPoolSize: 2,
-      maxPoolSize: 4,
+   mongoose.connect(process.env.DATABASE_URL_DEV, options, (err) => {
+      if (err) console.log(err);
+      else console.log('Connected to database');
    }))();
-
-/* export default mongoose.connect(process.env.DATABASE_URL, options, () => {
-   //if (err)
-});
- */
