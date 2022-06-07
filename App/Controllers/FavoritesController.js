33de8b2899d142e2c25fe759eac5cdfc4d3cc32e
rosariodeveloper <rosariomassango@gmail.com>
2022-06-07 14:51:06 +0100

    create favorites controller

--- App/Controllers/FavoritesController.js
+++ App/Controllers/FavoritesController.js
@@ -4,13 +4,21 @@ export class FavoritesController {
    static async index(req, res) {
       try {
          const favorites = await Favorites.find();
-         return res.status(500).json({
+
+         if (!favorites)
+            return res.status(404).json({
+               success: false,
+               message: 'Orders not found',
+            });
+
+         return res.status(200).json({
             success: true,
             data: favorites,
             message: 'Favorites retrieved successfully',
          });
       } catch (error) {
          return res.status(500).json({
+            error: error.message,
             success: false,
             message: 'Favorites not retrieved',
          });
