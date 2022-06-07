cc0464869487de0757f36257992cfff75070cf63
rosariodeveloper <rosariomassango@gmail.com>
2022-06-07 14:51:46 +0100

    create foods controller

--- App/Controllers/FoodsController.js
+++ App/Controllers/FoodsController.js
@@ -4,12 +4,20 @@ export class FoodsController {
    static async index(req, res) {
       try {
          const foods = await Foods.find();
+
+         if (!foods)
+            return res.status(404).json({
+               success: false,
+               message: 'Orders not found',
+            });
+
          return res.status(200).json({
             success: true,
             data: foods,
+            message: 'Foods retrieved successfully',
          });
       } catch (error) {
-         return res.status(422).json({
+         return res.status(500).json({
             success: false,
             message: 'Foods not retrieved. Try again later',
          });
