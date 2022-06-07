bb4056bb7e0fe736b9e1f0582bc84b8a7da58a04
rosariodeveloper <rosariomassango@gmail.com>
2022-06-07 23:24:53 +0100

    criando faqs request validator

--- App/Validators/CreateFaqsValidator.js
+++ App/Validators/CreateFaqsValidator.js
@@ -1,6 +1,9 @@
 //import { checkSchema } from 'express-validator';
 import * as yup from 'yup';
 import validator from '../Middleware/Validator.js';
+import validators from '../Services/ValidatorService.js';
+
+validators(yup);
 
 export const createFaqValidator = validator(
    yup.object({
