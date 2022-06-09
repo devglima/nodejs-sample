38865db091498aa72c761029d262a907b61c6cc6
rosariodeveloper <rosariomassango@gmail.com>
2022-06-09 19:28:58 +0100

    cart routes

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,11 +1,20 @@
 'use strict';
 
-import express from 'express';
+import { Router } from 'express';
 import { authenticate } from '../App/Middleware/AuthMiddleware.js';
 import { CartController } from '../App/Controllers/CartController.js';
 
-const Route = express.Router();
+const Route = Router();
 
-Route.get('/carts', authenticate, CartController.index);
+Route.get('/carts', authenticate, CartController.index)
+   .get('/carts/count', authenticate, CartController.count)
+   .post(
+      '/carts/payment-condition-price',
+      authenticate,
+      CartController.paymentConditionPrice
+   )
+   .post('/carts/create', authenticate, CartController.add)
+   .put('/carts/update', authenticate, CartController.add)
+   .delete('/carts/delete/:id', authenticate, CartController.delete);
 
 export default Route;
