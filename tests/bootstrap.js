f793cf82b82af0836e9a33af22abab141e3fe87d
rosariodeveloper <rosariomassango@gmail.com>
2022-06-09 19:33:05 +0100

    fix database error

--- tests/bootstrap.js
+++ tests/bootstrap.js
@@ -1,10 +1,19 @@
 import { resolve } from 'path';
+//import '../config/database.js';
 import request from 'supertest';
-import '../config/database.js';
 
 require('dotenv').config({
    path: resolve(__dirname, '..', '.env.test'),
 });
+
+export async function useAuth() {
+   const res = await Http.post('/login').send({
+      email: 'adm@mc1.com',
+      password: 'mc1#123',
+   });
+
+   return JSON.parse(res.text);
+}
 
 const HOST = `http://${process.env.HOST}:${process.env.PORT}`;
 export const Http = request(HOST);
