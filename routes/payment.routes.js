6e64f620d75324a3712e9026de4eb5f743d9096d
rosariodeveloper <rosariomassango@gmail.com>
2022-06-10 21:33:22 +0100

    payments routes

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,1 +1,17 @@
 'use strict';
+
+import { Router } from 'express';
+import { PaymentController } from '../App/Controllers/PaymentController.js';
+import { authenticate } from '../App/Middleware/AuthMiddleware.js';
+
+const Route = Router();
+
+Route.get('/payments', authenticate, PaymentController.index)
+   .post(
+      '/payments/parameters',
+      authenticate,
+      PaymentController.orderParameters
+   )
+   .post('/payments/pix', authenticate, PaymentController.pix);
+
+export default Route;
