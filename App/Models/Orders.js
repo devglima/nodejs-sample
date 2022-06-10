a51bf707a763c87b56ed20c37ec65db085f309c5
rosariodeveloper <rosariomassango@gmail.com>
2022-06-10 21:29:15 +0100

    orders model

--- App/Models/Orders.js
+++ App/Models/Orders.js
@@ -1,10 +1,29 @@
 'use strict';
 import mongoose from 'mongoose';
+import Inc from 'mongoose-sequence';
 
+const AutoIncrement = Inc(mongoose);
 const { Schema, model } = mongoose;
 
-const OrdersSchema = new Schema({});
+const OrdersSchema = new Schema(
+   {
+      user_id: { type: Number, required: true },
+      order_status_id: { type: Number, required: true },
+      tax: { type: Number },
+      hint: { type: String },
+      payment_id: { type: Number },
+      cIDOrder: { type: String },
+      xOrderStatus: { type: String },
+   },
+   {
+      timestamps: {
+         createdAt: 'created_at',
+         updatedAt: 'updated_at',
+      },
+   }
+);
 
+OrdersSchema.plugin(AutoIncrement, { id: 'order_id', inc_field: 'id' });
 const Orders = model('orders', OrdersSchema);
 
 export default Orders;
