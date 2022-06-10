a1dcdcac9f45e77f2e92160e64ea424073523f73
rosariodeveloper <rosariomassango@gmail.com>
2022-06-10 21:25:20 +0100

    Create payment controller

--- App/Controllers/PaymentController.js
+++ App/Controllers/PaymentController.js
@@ -1,6 +1,97 @@
-/* import PaymentModel from '../Models/Payment.js';
+import Payment from '../Models/Payments.js';
+import { PaymentRepository } from '../Repositories/PaymentRepository.js';
 
-class PaymentController {}
+export class PaymentController {
+   constructor() {}
 
-export default PaymentController;
- */
+   static async index(request, response) {
+      try {
+         const orders = await PaymentRepository.get();
+
+         return response.json({
+            success: true,
+            data: orders,
+            message: 'Payment retrieved successfully',
+         });
+      } catch (error) {
+         return response.status(500).json({
+            error: error.message,
+            success: false,
+            message: 'Could not process your request now. Try again later',
+         });
+      }
+   }
+
+   static async orderParameters(request, response) {
+      try {
+         const parameteres = await PaymentRepository.getParameters(
+            request.body
+         );
+
+         return response.json({
+            success: true,
+            data: parameteres,
+            message: 'Payment retrieved successfully',
+         });
+      } catch (error) {
+         return response.status(500).json({
+            error: error,
+            success: false,
+            message: 'Could not process your request now. Try again later',
+         });
+      }
+   }
+
+   static async pix(request, response) {
+      try {
+         const pix = await PaymentRepository.getPix(request.body);
+
+         return response.json({
+            success: true,
+            data: pix,
+            message: 'Payment retrieved successfully',
+         });
+      } catch (error) {
+         return response.status(500).json({
+            error: error,
+            success: false,
+            message: 'Could not process your request now. Try again later',
+         });
+      }
+   }
+
+   static async add(request, response) {
+      try {
+         const { user_id, cIDCompany, cIDProduct, quantity } = request.body;
+
+         const cart = await Payment.findOne({
+            user_id,
+            cIDCompany,
+            cIDProduct,
+         });
+
+         if (cart) {
+            cart.quantity += quantity;
+            await cart.save();
+         } else {
+            await Payment.create({
+               user_id,
+               cIDCompany,
+               cIDProduct,
+               quantity,
+            });
+         }
+
+         return response.json({
+            success: true,
+            message: 'Cart added successfully',
+         });
+      } catch (error) {
+         return response.status(500).json({
+            success: false,
+            message:
+               'Could not possible process your request now. Try again later',
+         });
+      }
+   }
+}
