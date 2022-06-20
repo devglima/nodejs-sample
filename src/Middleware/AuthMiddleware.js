dbfcac7bf5cb35936e96e42c26986b653353f0d2
rosariodeveloper <rosariomassango@gmail.com>
2022-06-20 13:13:05 +0100

    Fix: Check if user exist

--- src/Middleware/AuthMiddleware.js
+++ src/Middleware/AuthMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import auth from '../Utils/auth.js';
 
-export const authenticate = (req, res, next) => {
+export const authenticate = async (req, res, next) => {
    let header = req.headers['authorization'];
    header = header ? header.split(' ') : []; // ['Bearer', 'token']
 
@@ -12,16 +13,22 @@ export const authenticate = (req, res, next) => {
 
    // if there is no a bearer setence in the token
    if (header[0].toLowerCase() !== 'bearer')
-      return res.status(500).json({ auth: false, message: 'Invalid token' });
+      return res.status(500).json({ success: false, message: 'Invalid token' });
+
+   //Check if user exist
+   if ((await auth(req)) === null)
+      return res
+         .status(500)
+         .json({ success: false, message: 'Failed to authenticate user.' });
 
    const token = header[1]; //Get token string
 
    // verify token
-   jwt.verify(token, process.env.SECRET, function (err, decoded) {
+   jwt.verify(token, process.env.SECRET, function (err) {
       if (err)
          return res
             .status(500)
-            .json({ auth: false, message: 'Failed to authenticate token.' });
+            .json({ success: false, message: 'Failed to authenticate token.' });
 
       //req.body.id = decoded.id;
 
