b69315e187eea7bdae5533433d61097380310072
rosariodeveloper <rosariomassango@gmail.com>
2022-06-20 13:14:20 +0100

    Change food to product model

--- src/Models/Foods.js
+++ src/Models/Foods.js
@@ -1,69 +1,82 @@
 'use strict';
 import mongoose from 'mongoose';
+import Inc from 'mongoose-sequence';
+
+const AutoIncrement = Inc(mongoose);
 
 const { Schema, model } = mongoose;
 
-const FoodsSchema = new Schema({
-   id: {
-      type: Number,
+const ProductsSchema = new Schema(
+   {
+      name: {
+         type: String,
+         required: true,
+      },
+      price: {
+         type: Number,
+         required: true,
+      },
+      discount_price: {
+         type: Number,
+         required: true,
+      },
+      description: {
+         type: String,
+         required: true,
+      },
+      ingredients: {
+         type: String,
+      },
+      weight: {
+         type: Number,
+         required: true,
+      },
+      featured: {
+         type: Number,
+         required: true,
+      },
+      restaurant_id: {
+         type: Number,
+         required: true,
+      },
+      category_id: {
+         type: Number,
+         required: true,
+      },
+      created_at: {
+         type: String,
+      },
+      updated_at: {
+         type: String,
+      },
+      cIDCompany: {
+         type: String,
+         required: true,
+      },
+      cIDProduct: {
+         type: String,
+         required: true,
+      },
+      cImage: {
+         type: String,
+         required: true,
+      },
+      xIDUnitMeasureType: {
+         type: String,
+      },
    },
-   name: {
-      type: String,
-      required: true,
-   },
-   price: {
-      type: Number,
-      required: true,
-   },
-   discount_price: {
-      type: Number,
-      required: true,
-   },
-   description: {
-      type: String,
-      required: true,
-   },
-   ingredients: {
-      type: String,
-   },
-   weight: {
-      type: Number,
-      required: true,
-   },
-   featured: {
-      type: Number,
-      required: true,
-   },
-   restaurant_id: {
-      type: Number,
-      required: true,
-   },
-   category_id: {
-      type: Number,
-      required: true,
-   },
-   created_at: {
-      type: String,
-   },
-   updated_at: {
-      type: String,
-   },
-   cIDCompany: {
-      type: String,
-      required: true,
-   },
-   cIDProduct: {
-      type: String,
-      required: true,
-   },
-   cImage: {
-      type: String,
-      required: true,
-   },
-   xIDUnitMeasureType: {
-      type: String,
-   },
+   {
+      timestamps: {
+         createdAt: 'created_at',
+         updatedAt: 'updated_at',
+      },
+   }
+);
+
+ProductsSchema.plugin(AutoIncrement, {
+   id: 'order_status_id',
+   inc_field: 'id',
 });
 
-const Foods = model('foods', FoodsSchema);
-export default Foods;
+const Products = model('foods', ProductsSchema);
+export default Products;
