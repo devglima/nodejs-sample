cfd798b7db47a9a225c4abd31fb2685c0913f3f9
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2022-07-05 12:42:17 -0300

    Favorite repository

--- src/Repositories/FavoritesRepository.js
+++ src/Repositories/FavoritesRepository.js
@@ -8,10 +8,19 @@ export class FavoritesRepository {
       });
 
       favorites = favorites.map(async (favorite) => {
-         favorite.food = await Foods.find({cIDProduct: favorite.cIDProduct}).select({
+         favorite.food = await Foods.find({ cIDProduct: favorite.cIDProduct }).select({
             id: 1,
             name: 1,
             price: 1,
+            discountPrice: 1,
+            image: 1,
+            description: 1,
+            ingredients: 1,
+            weight: 1,
+            featured: 1,
+            cIDProduct: 1,
+            cIDCompany: 1,
+            xIDUnitMeasureType: 1,
          });
 
          return favorite;
