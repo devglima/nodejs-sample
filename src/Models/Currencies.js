16f8d8f4c5ce0ee852ae0031a505775b22e247c2
rosariodeveloper <rosariomassango@gmail.com>
2022-07-19 16:55:26 +0100

    adding paginate plugin

--- src/Models/Currencies.js
+++ src/Models/Currencies.js
@@ -2,9 +2,32 @@
 
 import mongoose from 'mongoose';
 
+import Inc from 'mongoose-sequence';
+import paginate from 'mongoose-paginate-v2';
+
+const AutoIncrement = Inc(mongoose);
+
 const { Schema, model } = mongoose;
 
-const CurrenciesSchema = new Schema({});
+const CurrenciesSchema = new Schema(
+   {
+      id: { type: Number, required: true },
+      name: { type: String, required: true },
+      code: { type: String, required: true },
+      symbol: { type: String, required: true },
+      decimal_digits: { type: String, required: true },
+      rounding: { type: String, required: true },
+   },
+   {
+      timestamps: {
+         createdAt: 'created_at',
+         updatedAt: 'updated_at',
+      },
+   }
+);
+
+CurrenciesSchema.plugin(AutoIncrement, { id: 'currency_id', inc_field: 'id' });
+CurrenciesSchema.plugin(paginate);
 
 const Currencies = model('currencies', CurrenciesSchema);
 
