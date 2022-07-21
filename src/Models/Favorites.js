0241a30a5b58e5d112f662b48c7250f89c5e4eb9
rosariodeveloper <rosariomassango@gmail.com>
2022-07-21 15:31:40 +0100

    foods

--- src/Models/Favorites.js
+++ src/Models/Favorites.js
@@ -1,35 +1,47 @@
 'use strict';
 import mongoose from 'mongoose';
+import Inc from 'mongoose-sequence';
 
 const { Schema, model } = mongoose;
 
-const FavoritesSchema = new Schema({
-   id: {
-      type: Number,
-      required: true,
-   },
-   user_id: {
-      type: mongoose.Types.ObjectId,
-      required: true,
-   },
-   cIDCompany: {
-      type: String,
-      required: true,
-   },
-   cIDProduct: {
-      type: String,
-      required: true,
-   },
-   created_at: {
-      type: String,
-      required: true,
-   },
-   updated_at: {
-      type: String,
-      required: true,
-   },
-});
+const AutoIncrement = Inc(mongoose);
 
+const FavoritesSchema = new Schema(
+   {
+      id: {
+         type: Number,
+         required: true,
+      },
+      user_id: {
+         type: mongoose.Types.ObjectId,
+         required: true,
+      },
+      cIDCompany: {
+         type: String,
+         required: true,
+      },
+      cIDProduct: {
+         type: String,
+         required: true,
+      },
+      created_at: {
+         type: String,
+         required: true,
+      },
+      updated_at: {
+         type: String,
+         required: true,
+      },
+   },
+   {
+      timestamps: {
+         createdAt: 'created_at',
+         updatedAt: 'updated_at',
+      },
+   }
+);
+
+FavoritesSchema.plugin(AutoIncrement, { id: 'favorite_id', inc_field: 'id' });
 const Favorites = model('favorites', FavoritesSchema);
 
 export default Favorites;
