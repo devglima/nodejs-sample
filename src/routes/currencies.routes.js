afd91286bbd027db088e26de11d1cd9b109a83ea
rosariodeveloper <rosariomassango@gmail.com>
2022-08-06 01:50:04 +0100

    Feat: Custom validate middleware

--- src/routes/currencies.routes.js
+++ src/routes/currencies.routes.js
@@ -1,15 +1,24 @@
 'use strict';
 
 import express from 'express';
+import { currencyValidator } from '../Validators/CurrencyValidator.js';
 import { CurrenciesController } from './../Controllers/CurrenciesController.js';
 import { authenticate } from './../Middleware/AuthMiddleware.js';
 
 const Route = express.Router();
 
 Route.get('/currencies', authenticate, CurrenciesController.index)
-   .post('/currencies/create', authenticate, CurrenciesController.create)
+   .post(
+      '/currencies/create',
+      [authenticate, currencyValidator],
+      CurrenciesController.create
+   )
    .get('/currencies/:id', authenticate, CurrenciesController.show)
-   .put('/currencies/update/:id', authenticate, CurrenciesController.update)
+   .put(
+      '/currencies/update/:id',
+      [authenticate, currencyValidator],
+      CurrenciesController.update
+   )
    .delete('/currencies/delete/:id', authenticate, CurrenciesController.delete);
 
 export default Route;
