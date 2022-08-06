5042ad1ee77793ef6fa61b5d7af1551db79895ec
rosariodeveloper <rosariomassango@gmail.com>
2022-08-06 01:48:20 +0100

    Chore: Set paginate plugin

--- src/Models/Categories.js
+++ src/Models/Categories.js
@@ -1,24 +1,31 @@
 import mongoose from 'mongoose';
+import Inc from 'mongoose-sequence';
+import paginate from 'mongoose-paginate-v2';
 
+const AutoIncrement = Inc(mongoose);
 const { Schema, model } = mongoose;
 
 const CategoriesSchema = new Schema(
    {
-      id: {
-         type: Number,
-      },
+      id: { type: Number },
+      image: { type: String },
       name: {
          type: String,
          required: true,
       },
+      description: { type: String },
    },
    {
       timestamps: {
          createdAt: 'created_at',
          updatedAt: 'updated_at',
       },
+      versionKey: false,
    }
 );
+
+CategoriesSchema.plugin(AutoIncrement, { id: 'category_id', inc_field: 'id' });
+CategoriesSchema.plugin(paginate);
 
 const Categories = model('categories', CategoriesSchema);
 export default Categories;
