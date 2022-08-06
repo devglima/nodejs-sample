2d81a9b86ecff6fd873aa9022687fa891f01ca6f
rosariodeveloper <rosariomassango@gmail.com>
2022-08-06 01:55:37 +0100

    Chore: Set paginate plugin

--- src/Models/user.js
+++ src/Models/user.js
@@ -2,40 +2,52 @@
 
 import mongoose from 'mongoose';
 import Inc from 'mongoose-sequence';
+import paginate from 'mongoose-paginate-v2';
 
 const AutoIncrement = Inc(mongoose);
+
 const { Schema, model } = mongoose;
 
-const UserSchema = new Schema({
-   name: {
-      type: String,
-      required: true,
+const UserSchema = new Schema(
+   {
+      name: {
+         type: String,
+         required: true,
+      },
+      email: {
+         type: String,
+         required: true,
+      },
+      password: {
+         type: String,
+         required: true,
+      },
+      device_token: {
+         type: String,
+      },
+      device_chosen_language: {
+         type: String,
+      },
+      custom_fields: {
+         type: Object,
+      },
+      has_media: {
+         type: Boolean,
+      },
+      media: {
+         type: Array,
+      },
    },
-   email: {
-      type: String,
-      required: true,
-   },
-   password: {
-      type: String,
-      required: true,
-   },
-   device_token: {
-      type: String,
-   },
-   device_chosen_language: {
-      type: String,
-   },
-   custom_fields: {
-      type: Object,
-   },
-   has_media: {
-      type: Boolean,
-   },
-   media: {
-      type: Array,
-   },
-});
+   {
+      timestamps: {
+         createdAt: 'created_at',
+         updatedAt: 'updated_at',
+      },
+   }
+);
 
 UserSchema.plugin(AutoIncrement, { id: 'user_id', inc_field: 'id' });
+UserSchema.plugin(paginate);
+
 const User = model('users', UserSchema);
 export default User;
